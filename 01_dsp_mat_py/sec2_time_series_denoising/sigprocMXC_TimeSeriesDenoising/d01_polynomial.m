a0357da5d5b78c6e702abc4e2660aea1fd15d7c3
Jaehoon Jeong <rosejeongj1@gmail.com>
2025-03-23 00:30:09 +0900

    Bayes information criterion (BIC) added

--- 01_dsp_mat_py/sec2_time_series_denoising/sigprocMXC_TimeSeriesDenoising/d01_polynomial.m
+++ 01_dsp_mat_py/sec2_time_series_denoising/sigprocMXC_TimeSeriesDenoising/d01_polynomial.m
@@ -45,3 +45,58 @@ plot(t,residual,'k','linew',2)
 
 legend({'Original';'Polyfit';'Filtered signal'})
 
+%% Bayes information criterion to find optimal order
+
+% possible orders
+orders = (5:40)';
+
+% sum of squared errors (sse is reserved!)
+sse1 = zeros(length(orders),1); 
+
+% loop through orders
+for ri=1:length(orders)
+    
+    % compute polynomial (fitting time series)
+    yHat = polyval(polyfit(t,signal,orders(ri)),t);
+    
+    % compute fit of model to data (sum of squared errors)
+    sse1(ri) = sum( (yHat-signal).^2 )/n;
+end
+
+% Bayes information criterion
+bic = n*log(sse1) + orders*log(n);
+
+% best parameter has lowest BIC
+[bestP,idx] = min(bic);
+
+% would continue getting smaller without adding parameters
+
+% plot the BIC
+figure(4), clf, hold on
+plot(orders,bic,'ks-','markerfacecolor','w','markersize',8)
+plot(orders(idx),bestP,'ro','markersize',10,'markerfacecolor','r')
+xlabel('Polynomial order'), ylabel('Bayes information criterion')
+zoom on
+
+%% now repeat filter for best (smallest) BIC
+
+% polynomial fit
+polycoefs = polyfit(t,signal,orders(idx));
+
+% estimated data based on the coefficients
+yHat = polyval(polycoefs,t);
+
+% filtered signal is residual
+filtsig = signal - yHat;
+
+
+%%% plotting
+figure(5), clf, hold on
+h = plot(t,signal);
+set(h,'color',[1 1 1]*.6)
+plot(t,yHat,'r','linew',2)
+plot(t,filtsig,'k')
+set(gca,'xlim',t([1 end]))
+
+xlabel('Time (a.u.)'), ylabel('Amplitude')
+legend({'Original';'Polynomial fit';'Filtered'})
